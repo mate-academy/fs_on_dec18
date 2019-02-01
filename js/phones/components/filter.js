a0c20f08950e0e0afc77daaff96564ddf321fdf6
mgrinko <glukym@gmail.com>
2019-02-01 11:31:02 +0200

    lodash.debounce was added to filter

--- js/phones/components/filter.js
+++ js/phones/components/filter.js
@@ -1,10 +1,31 @@
 import Component from '../../component.js';
 
+let debounce = _.debounce;
+
+const QUERY_CHANGE_DELAY = 300;
+
+console.log(debounce);
+
 export default class Filter extends Component {
   constructor({ element }) {
     super({ element });
 
     this._render();
+
+    this._queryField = this._element.querySelector('[data-element="query-field"]');
+    this._orderField = this._element.querySelector('[data-element="order-field"]');
+
+    this.on('change', 'order-field', () => {
+      console.log(this._orderField.value);
+      this.emit('order-changed', this._orderField.value);
+    });
+
+    const emitQueryChangedWithDebounce = debounce(() => {
+      console.log(this._queryField.value);
+      this.emit('query-changed', this._orderField.value);
+    }, QUERY_CHANGE_DELAY);
+
+    this.on('input', 'query-field', emitQueryChangedWithDebounce);
   }
 
   _render() {
