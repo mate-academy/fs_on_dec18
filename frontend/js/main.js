62fae31298891c28764a0541229b46c4aa7b3805
mgrinko <glukym@gmail.com>
2019-02-14 11:27:30 +0200

    CSS loader was added

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,7 @@
 import PhonesPage from './phones/phones-page.js';
+import './main.css';
 
 // eslint-disable-next-line no-unused-vars
 const currentPage = new PhonesPage({
   element: document.querySelector('[data-page-container]'),
 });
-
-alert(456);
